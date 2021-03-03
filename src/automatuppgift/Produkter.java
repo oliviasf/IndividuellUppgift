fcb660fdb0737f7dc8366f0155769a40b1cf6038
Olivia Svensk <olivia.svensk.franzen.mte21lin@tucsweden.se>
2021-03-03 11:56:46 +0100

    Lägger till en rad

---
+++ src/automatuppgift/Produkter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automatuppgift;
+
+/**
+ *
+ * @author olivi
+ */
+abstract public class Produkter {
+    public String name;
+    public int price;
+    public String description;
+
+
+//Skapar konstruktor
+public Produkter(String name, int price, String description);{
+    this.name = name;
+    this.price = price;
+    this.description = description;
+}
+}
